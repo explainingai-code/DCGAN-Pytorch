df69681e13336c5e505ace1bc261c4227785ffe5
akansh12 <akanshmaurya@gmail.com>
2024-01-10 15:33:51 +0100

    model_celebA_done

--- model/dcgans.py
+++ model/dcgans.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import torch.nn as nn
 
 
@@ -9,63 +10,80 @@ and
 https://arxiv.org/pdf/1511.06434.pdf
 
 '''
+latent_dim = 100
+in_channels = [512, 256, 128, 64]
+kernel_size = [4,4,4,4,4]
+stride = [1,2,2,2,2]
+padding = [0,1,1,1,1]
+out_channels = 3
+
+
 class Generator(nn.Module):
-    def __init__(self,latent_dim):
+    def __init__(self,latent_dim = latent_dim, in_channels = in_channels, kernel_size = kernel_size, stride = stride, padding = padding, out_channels = out_channels):
         super(Generator, self).__init__()
         self.latent_dim = latent_dim
+        self.in_channels = in_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.out_channels = out_channels
 
         self.main = nn.Sequential(
             ###
-            nn.ConvTranspose2d(self.latent_dim,1024,4, 1, 0, bias=False),
-            nn.BatchNorm2d(1024),
+            nn.ConvTranspose2d(self.latent_dim,self.in_channels[0],self.kernel_size[0], self.stride[0], self.padding[0], bias=False),
+            nn.BatchNorm2d(self.in_channels[0]),
             nn.ReLU(True),
             ###
-            nn.ConvTranspose2d(1024,512,5, 2, 1, bias=False),
-            nn.BatchNorm2d(512),
+            nn.ConvTranspose2d(self.in_channels[0],self.in_channels[1],self.kernel_size[1], self.stride[1], self.padding[1], bias=False),
+            nn.BatchNorm2d(self.in_channels[1]),
             nn.ReLU(True),
             ###
-            nn.ConvTranspose2d(512,256,5, 2, 1, bias=False),
-            nn.BatchNorm2d(256),
+            nn.ConvTranspose2d(self.in_channels[1],self.in_channels[2],self.kernel_size[2], self.stride[2], self.padding[2], bias=False),
+            nn.BatchNorm2d(self.in_channels[2]),
             nn.ReLU(True),
             ###
-            nn.ConvTranspose2d(256,128,5, 2, 1, bias=False),
-            nn.BatchNorm2d(128),
+            nn.ConvTranspose2d(self.in_channels[2],self.in_channels[3],self.kernel_size[3], self.stride[3], self.padding[3], bias=False),
+            nn.BatchNorm2d(self.in_channels[3]),
             nn.ReLU(True),
             ###
-            nn.ConvTranspose2d(128,3,5, 2, 1, bias=False),
+            nn.ConvTranspose2d(self.in_channels[3],self.out_channels,self.kernel_size[4], self.stride[4], self.padding[4], bias=False),
             nn.Tanh()
-
         )
+
     def forward(self, input):
         return self.main(input)
     
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels = in_channels, kernel_size = kernel_size, stride = stride, padding = padding, out_channels = out_channels):
         super(Discriminator, self).__init__()
+        self.in_channels = np.flip(in_channels)
+        self.kernel_size = np.flip(kernel_size)
+        self.stride = np.flip(stride)
+        self.padding = np.flip(padding)
+        self.out_channels = out_channels
 
         self.main = nn.Sequential(
             ###
-            nn.Conv2d(3,128,5, 2, 1, bias=False),
+            nn.Conv2d(self.out_channels,self.in_channels[0],self.kernel_size[0], self.stride[0], self.padding[0], bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             ###
-            nn.Conv2d(128,256,5, 2, 1, bias=False),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(self.in_channels[0],self.in_channels[1],self.kernel_size[1], self.stride[1], self.padding[1], bias=False),
+            nn.BatchNorm2d(self.in_channels[1]),
             nn.LeakyReLU(0.2, inplace=True),
             ###
-            nn.Conv2d(256,512,5, 2, 1, bias=False),
-            nn.BatchNorm2d(512),
+            nn.Conv2d(self.in_channels[1],self.in_channels[2],self.kernel_size[2], self.stride[2], self.padding[2], bias=False),
+            nn.BatchNorm2d(self.in_channels[2]),
             nn.LeakyReLU(0.2, inplace=True),
             ###
-            nn.Conv2d(512,1024,5, 2, 1, bias=False),
-            nn.BatchNorm2d(1024),
+            nn.Conv2d(self.in_channels[2],self.in_channels[3],self.kernel_size[3], self.stride[3], self.padding[3], bias=False),
+            nn.BatchNorm2d(self.in_channels[3]),
             nn.LeakyReLU(0.2, inplace=True),
             ###
-            nn.Conv2d(1024,1,4, 1, 0, bias=False),
+            nn.Conv2d(self.in_channels[3],1,self.kernel_size[4], self.stride[4], self.padding[4], bias=False),
             nn.Sigmoid()
         )
     def forward(self, input):
         return self.main(input)
-    
     
 
 
